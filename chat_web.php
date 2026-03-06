e3a5b646acc01444c3c2ac610de963a0ec4b25c5
narciso97br <narciso1997@gmail.com>
2026-03-06 09:49:51 -0300

    00182

--- chat_web.php
+++ chat_web.php
@@ -1549,7 +1549,7 @@ echo '      alert("Erro ao enviar convite: " + e.message);';
 echo '    })';
 echo '    .finally(() => {';
 echo '      btn.disabled = false;';
-echo '      btn.innerHTML = \'<svg width=\"16\" height=\"16\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" style=\"display:inline-block;vertical-align:middle;margin-right:6px\"><path d=\"M17 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2\"/><circle cx=\"9\" cy=\"7\" r=\"4\"/><path d=\"M23 21v-2a4 4 0 00-3-3.87M16 3.13a4 4 0 010 7.75\"/></svg>Enviar Convite\';';';
+echo '      btn.innerHTML = \'<svg width=\"16\" height=\"16\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" style=\"display:inline-block;vertical-align:middle;margin-right:6px\"><path d=\"M17 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2\"/><circle cx=\"9\" cy=\"7\" r=\"4\"/><path d=\"M23 21v-2a4 4 0 00-3-3.87M16 3.13a4 4 0 010 7.75\"/></svg>Enviar Convite\';';
 echo '    });';
 echo '  }';
 echo '}';
