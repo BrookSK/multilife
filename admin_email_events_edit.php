82f1419030222f87df364cad3b72ac5af3ff6820
narciso97br <narciso1997@gmail.com>
2026-03-08 23:07:59 -0300

    00425

--- admin_email_events_edit.php
+++ admin_email_events_edit.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 require_once __DIR__ . '/app/bootstrap.php';
 
 auth_require_login();
-rbac_require_permission('email.manage');
+rbac_require_permission('admin.settings.manage');
 
 $eventId = isset($_GET['id']) ? (int)$_GET['id'] : null;
 $pageTitle = $eventId ? 'Editar Evento de E-mail' : 'Criar Evento de E-mail';
